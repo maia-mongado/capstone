a48f57eb3a714058184d9a21b3418c6035754529
Maia Mongado <maiamongado@Maias-MacBook-Pro.local>
2023-01-10 22:35:30 -0500

    added scrolling to top

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import './App.css';
-import ReactDOM from "react-dom";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout.js"
 import Home from "./components/Home.js"
 import Services from "./components/Services.js"
@@ -28,7 +27,6 @@ import PainManagement from './components/services/PainManagement.js';
 function App() {
   return (
     <div className="App">
-      <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -54,90 +52,6 @@ function App() {
 
         </Route>
       </Routes>
-    </HashRouter>
-
-    <footer>
-        <div id="footer-info">
-            <div id="hours">
-                <h3>Hours</h3>
-                <table>
-                    <tr>
-                        <td><b>Sunday:</b></td>
-                        <th class="space"></th>
-                        <td>closed</td>
-                    </tr>
-                    <tr class="blank_row">
-                        <td colspan="3"></td>
-                    </tr>
-                    <tr>
-                        <td><b>Monday to Thursday:</b></td>
-                        <th class="space"></th>
-                        <td>7:30 am - 12:00 pm</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <th class="space"></th>
-                        <td>1:30 pm - 6:30 pm</td>
-                    </tr>
-                    <tr class="blank_row">
-                        <td colspan="3"></td>
-                    </tr>
-                    <tr>
-                        <td><b>Friday:</b></td>
-                        <th class="space"></th>
-                        <td>7:30 am - 12:00 pm</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <th class="space"></th>
-                        <td>1:30 pm - 7:00 pm</td>
-                    </tr>
-                    <tr class="blank_row">
-                        <td colspan="3"></td>
-                    </tr>
-                    <tr>
-                        <td><b>Saturday:</b></td>
-                        <th class="space"></th>
-                        <td>7:30 am - 12:00 pm</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <th class="space"></th>
-                        <td>1:00 pm - 4:00 pm</td>
-                    </tr>
-                </table>
-            </div>
-            <div id="location">
-                <h3>Location</h3>
-                <img src={process.env.PUBLIC_URL + "/images/home/map.png"} />
-                <p>265 Gano Street
-                    <br></br>
-                    Providence, RI  02906
-                    <br></br>
-                    <br></br>
-                    <b>Phone:</b> 401-272-2345
-                    <br></br>
-                    <b>Fax:</b> 401-272-1305
-                </p>
-            </div>
-        </div>
-        
-        <hr></hr>
-        
-        <div id="symbols">
-            <div id="icons">
-              <a href='https://www.facebook.com/EastSideVeterinaryClinic' target='_blank'>
-                <img id="facebook" src={process.env.PUBLIC_URL +"/images/home/facebooklogo.png"} />
-              </a>
-              <a href='https://www.petly.com' target='_blank'>
-                <img id="petly" src={process.env.PUBLIC_URL+ "/images/home/petly-bubble.png"} />
-              </a>
-            </div>
-            <p><b>We accept:</b></p>
-            <img id="credit-cards" src={process.env.PUBLIC_URL + "/images/home/credit-cards.png"} />
-            <img id="insurance" src={process.env.PUBLIC_URL + "/images/home/insurance.png"} />
-        </div>       
-    </footer>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
+import ScrollToTop from './util/ScrollToTop.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <ScrollToTop />
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
